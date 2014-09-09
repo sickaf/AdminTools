b40dfc9fa24de9dfe53121906c5c690b52cb733c
devon townsend <devonstownsend@gmail.com>
2014-09-09 20:19:10 +0800

    added ios project

---
+++ ios/SickAF MGR/SickAF MGRTests/SickAF_MGRTests.swift
@@ -0,0 +1,36 @@
+//
+//  SickAF_MGRTests.swift
+//  SickAF MGRTests
+//
+//  Created by dtown on 9/9/14.
+//  Copyright (c) 2014 sickaf. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SickAF_MGRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
