9969e8df319bd0afecd127ee0d057873e7ef015d
devon townsend <devonstownsend@gmail.com>
2014-09-10 19:18:37 +0800

    now all HTTP requests are chained when posting instagram photo to parse

--- ios/SickAF MGR/SickAF MGR/ViewController.swift
+++ ios/SickAF MGR/SickAF MGR/ViewController.swift
@@ -22,16 +22,14 @@ class ViewController: UIViewController {
     var username = ""
     var forDate = "2015-01-01"
     
-    @IBAction func getButton(sender: UIButton) {
+    @IBAction func postButton(sender: UIButton) {
         
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        var responseString = ""
         forDate = dateFormatter.stringFromDate(dateView.date)
         consoleLabel.text = forDate
-        
         Alamofire.request(.GET, urlTextField.text )
             .responseString { (request, response, string, error) in
-                responseString = string!
+                var responseString = string!
                 var userString = responseString
                 responseString = responseString.componentsSeparatedByString("og:image\" content=\"")[1]
                 responseString = responseString.componentsSeparatedByString("\"")[0]
@@ -43,24 +41,20 @@ class ViewController: UIViewController {
                 println("username: \(userString)")
                 self.username = userString
                 
-        }
-    }
-    
-    @IBAction func postButton(sender: UIButton) {
-        
-        Alamofire.Manager.sharedInstance.defaultHeaders.updateValue("p8AF2BKCLQ7fr3oJXPg43fOL6LXAK3mwAb5Ywnke", forKey: "X-Parse-Application-Id")
-        Alamofire.Manager.sharedInstance.defaultHeaders.updateValue("v8C3jQHw0b8JkoCMy3Vn9QgqLdl3F7TxptAKfSVx", forKey: "X-Parse-REST-API-Key")
-        Alamofire.Manager.sharedInstance.defaultHeaders.updateValue("application/json", forKey: "Content-Type")
-
-        let parameters : [ String : AnyObject] = [
-            "URL": self.photoUrl,
-            "forDate": forDate,
-            "IGUsername": self.username
-        ]
-        
-        Alamofire.request(.POST, "https://api.parse.com/1/classes/IGPhoto", parameters: parameters, encoding: .JSON)
-            .responseJSON { (request, response, JSON, error) in
-                println("bytes read \(JSON)")
+                Alamofire.Manager.sharedInstance.defaultHeaders.updateValue("p8AF2BKCLQ7fr3oJXPg43fOL6LXAK3mwAb5Ywnke", forKey: "X-Parse-Application-Id")
+                Alamofire.Manager.sharedInstance.defaultHeaders.updateValue("v8C3jQHw0b8JkoCMy3Vn9QgqLdl3F7TxptAKfSVx", forKey: "X-Parse-REST-API-Key")
+                Alamofire.Manager.sharedInstance.defaultHeaders.updateValue("application/json", forKey: "Content-Type")
+                
+                let parameters : [ String : AnyObject] = [
+                    "URL": self.photoUrl,
+                    "forDate": self.forDate,
+                    "IGUsername": self.username
+                ]
+                
+                Alamofire.request(.POST, "https://api.parse.com/1/classes/IGPhoto", parameters: parameters, encoding: .JSON)
+                    .responseJSON { (request, response, JSON, error) in
+                        println("Parse response: \(JSON)")
+                }
         }
     }
     
